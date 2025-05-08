94ddd91f4a09b803e9e96a734ee4bf25b5ae2043
Sta Makro <s.makrodimitris@erasmusmc.nl>
2025-05-08 09:35:25 +0200

    added coding for mixing raw files

---
+++ src/python/convertCounts2genes.py
@@ -0,0 +1,316 @@
+from pathlib import Path
+import sys
+import pandas as pd
+from copy import deepcopy
+import numpy as np
+import os
+import pickle
+import warnings
+
+def createPath(inputPath):
+	srcPath = Path(__file__).parent
+	return (srcPath / inputPath).resolve()
+
+
+def generateSite2tss():
+	genes = pd.read_table(createPath('../resources/2kbTSS.tsv'))
+
+	sites = np.array(genes['SiteIds'])
+	siteCounts = np.array(genes['SiteCount'])
+
+	geneNames =  list(genes['Chromosome']+':'+genes['Start'].astype(str) + '-' + genes['End'].astype(str))
+	uniqueGeneNames = []
+	geneSet = set()
+	for g in geneNames:
+		if g not in geneSet:
+			uniqueGeneNames.append(g)
+			geneSet.add(g)
+
+	#uniqueGenes = np.unique(geneNames)
+
+	g2i = dict()
+	for i, g in enumerate(uniqueGenes):
+		g2i[g] = i
+
+	mapping = dict()
+	for i, (s, sc, g) in enumerate(zip(sites, siteCounts, geneNames)):
+
+		if s != 'EMPTY;':
+			ss = s.split(';')
+			start = int(ss[0])
+			try:
+				end = int(ss[1])
+			except ValueError:
+				assert sc == 1 or sc == 0
+				end = start
+		else:
+			start = -1
+			end = -1
+
+
+		if g in mapping:
+			assert mapping[g][0] == start
+			assert mapping[g][1] == end
+		else:
+			mapping[g] = [start, end, g2i[g]]
+
+
+	with open(createPath('../resources/site2tss.pkl'), 'wb') as f:
+		pickle.dump(mapping, f)
+
+
+
+def generateSite2mhb():
+	genes = pd.read_table(createPath('../resources/mhb-medseq-overlap.txt'), header=None, usecols=[4,5,6,7], index_col=0)
+
+	geneNames =  list(genes[5]+':'+genes[6].astype(str) + '-' + genes[7].astype(str))
+	genes[1] = genes[5]+':'+genes[6].astype(str) + '-' + genes[7].astype(str)
+	genes.drop([5,6,7], axis=1, inplace=True)
+	uniqueGenes = np.unique(geneNames)
+
+	g2i = dict()
+	for i, g in enumerate(uniqueGenes):
+		g2i[g] = i
+
+	mapping = dict()
+
+	for i, gg in zip(genes.index, genes[1]):
+		if gg not in mapping:
+			mapping[gg] = [g2i[gg], i]
+		else:
+			mapping[gg].append(i)
+
+
+	with open('resources/site2mhb.pkl', 'wb') as f:
+		pickle.dump(mapping, f)
+
+def generateSite2cpgIsland():
+	mapping = dict()
+
+	with open(createPath('../resources/cpg_islands_intersect_lpnp1_sites.txt')) as f:
+		for line in f:
+			fields = line.split('\t')
+
+			island = fields[0] + ':' + fields[1] + '-' + fields[2]
+
+			rcSite = int(fields[-1])
+
+			if rcSite == -1:
+				continue
+
+			if island in mapping:
+				mapping[island].append(rcSite)
+			else:
+				mapping[island] = [rcSite]
+
+	with open(createPath('../resources/site2island.pkl'), 'wb') as f:
+		pickle.dump(mapping, f)
+
+
+
+def convertCounts2tss(rawCounts, ind=None):
+
+	if ind is None:
+		ind = [0, 1, 2, 3]
+
+	try:
+		with open(createPath('../resources/site2tss.pkl'), 'rb') as f:
+			mapping = pickle.load(f)
+	except FileNotFoundError:
+			generateSite2tss()
+			with open(createPath('../resources/site2tss.pkl'), 'rb') as f:
+				mapping = pickle.load(f)
+
+	genes = pd.read_table(createPath('../resources/2kbTSS.tsv'))
+
+	transformedCounts = np.zeros((len(mapping), 4))
+
+	geneNames = ['' for _ in range(len(mapping))]
+
+	for key in mapping:
+		start, end, index = mapping[key]
+
+
+		geneNames[index] = key
+		if start > -1 and end > -1:
+			tmp = rawCounts[start:(end+1)]
+			transformedCounts[index] = [np.sum(tmp), np.max(tmp), np.min(tmp), np.sum(tmp > 0)]
+		else:
+			transformedCounts[index] = [np.nan, np.nan, np.nan, np.nan]
+
+
+	result = pd.DataFrame(data=transformedCounts, index=geneNames, columns=['sum', 'max', 'min', 'nsitesActive'])
+	result = result.iloc[:, ind]
+
+	return result
+
+
+def convertCounts2mhb(rawCounts, ind=None):
+
+	if ind is None:
+		ind = [0, 1, 2, 3, 4, 5]
+
+	try:
+		with open(createPath('../resources/site2mhb.pkl'), 'rb') as f:
+			mapping = pickle.load(f)
+	except FileNotFoundError:
+			generateSite2mhb()
+			with open(createPath('resources/site2mhb.pkl'), 'rb') as f:
+				mapping = pickle.load(f)
+
+	transformedCounts = np.zeros((len(mapping), 6))
+
+	geneNames = ['' for _ in range(len(mapping))]
+
+	keys = sorted(list(mapping.keys()))
+
+	for key in keys:
+		loc = mapping[key][0]
+		index = mapping[key][1:]
+
+		geneNames[loc] = key
+
+		tmp = rawCounts[index]
+		transformedCounts[loc] = [np.sum(tmp), np.max(tmp), np.min(tmp), np.sum(tmp > 0), np.mean(tmp), np.std(tmp, ddof=1)]
+
+	result = pd.DataFrame(data=transformedCounts, index=geneNames, columns=['sum', 'max', 'min', 'nsitesActive', 'mean', 'std'])
+	result = result.iloc[:, ind]
+
+	return result
+
+
+
+def convertCounts2CpGisland(rawCounts, ind=None):
+
+	if ind is None:
+		ind = [0, 1, 2, 3]
+
+	try:
+		with open(createPath('../resources/site2island.pkl'), 'rb') as f:
+			mapping = pickle.load(f)
+	except FileNotFoundError:
+			generateSite2cpgIsland()
+			with open(createPath('../resources/site2island.pkl'), 'rb') as f:
+				mapping = pickle.load(f)
+
+	genes = pd.read_table(createPath('../resources/cpg_islands_grch38_chromosomes_sorted.bed'), names=["chrom", "start", "end", "name"])
+
+	genes["ID"] = genes["chrom"] + ":" + genes["start"].astype(str) + '-' + genes["end"].astype(str)
+	genes.drop('name', axis=1, inplace=True)
+	genes.set_index('ID', inplace=True)
+
+
+	transformedCounts = np.zeros((len(mapping), 4))
+
+	geneNames = ['' for _ in range(len(mapping))]
+
+	for ii, key in enumerate(sorted(mapping)):
+		cpgInds = mapping[key]
+
+
+		geneNames[ii] = key
+
+		tmp = rawCounts[cpgInds]
+		transformedCounts[ii] = [np.sum(tmp), np.max(tmp), np.min(tmp), np.sum(tmp > 0)]
+
+	result = pd.DataFrame(data=transformedCounts, index=geneNames, columns=['sum', 'max', 'min', 'nsitesActive'])
+	result = result.iloc[:, ind]
+
+	return result
+
+
+def convertCounts2clusteredRegions(rawCounts, ind=None):
+
+	if ind is None:
+		ind = [0, 1, 2, 3]
+
+	try:
+		with open(createPath('../resources/site2cluster.pkl'), 'rb') as f:
+			mapping = pickle.load(f)
+	except FileNotFoundError:
+			# TODO : not implemented in codebase
+			generateSite2cluster()
+			with open(createPath('resources/site2cluster.pkl'), 'rb') as f:
+				mapping = pickle.load(f)
+
+	transformedCounts = np.zeros((len(mapping), 4))
+
+	geneNames = ['' for _ in range(len(mapping))]
+
+	for ii, key in enumerate(sorted(mapping)):
+		cpgInds = mapping[key]
+		geneNames[ii] = key
+
+		tmp = rawCounts[cpgInds]
+		transformedCounts[ii] = [np.sum(tmp), np.max(tmp), np.min(tmp), np.sum(tmp > 0)]
+
+	result = pd.DataFrame(data=transformedCounts, index=geneNames, columns=['sum', 'max', 'min', 'nsitesActive'])
+	result = result.iloc[:, ind]
+
+	return result
+
+
+
+def convertSite2all(rcSiteFile, rcsite=False, cpgi=False, tss=False, mhb=False, clus=False, ind=None, return_library_size=True):
+	assert (rcsite or cpgi or tss or mhb or clusters), 'At least one of rcsite, cpgi, tss, mhb must be True'
+
+	rawCounts = np.array(pd.read_table(rcSiteFile, header=None, usecols=[0])).reshape(-1,)
+	returnDict = dict()
+
+	if rcsite:
+		returnDict['rcsite'] = rawCounts
+
+	if return_library_size:
+		returnDict['libsize'] = np.sum(rawCounts)
+
+	if cpgi:
+		returnDict['cpgi'] = convertCounts2CpGisland(rawCounts, ind)
+
+	if tss:
+		returnDict['tss'] = convertCounts2tss(rawCounts, ind)
+
+	if mhb:
+		returnDict['mhb'] = convertCounts2mhb(rawCounts, ind)
+
+	if clus:
+		returnDict['clus'] = convertCounts2clusteredRegions(rawCounts, ind)
+
+
+	return returnDict
+
+
+# these are test functions to make sure the 2kbTSS code works; they use data that are not available
+# TODO: delete for public release?
+def verify():
+	warnings.warn("deprecated", DeprecationWarning)
+	prefix = '../cf-DNA-Teoman-Stavros-data/Reads-per-LpnPI-CpG/'
+	files = os.listdir(prefix)
+
+	for i, file in enumerate(files):
+		print('%d/%d' % (i, len(files)), flush=True)
+		nn = file.split('_')[0]
+
+		example = pd.read_table('../cfDNA-Teoman/2kbTSS/' + nn + '_1kbTSS1kb_Lpnp1CpG_ReadCount.txt')
+
+		result = convert(prefix + file)
+
+		print('Converted. Now checking...')
+		assert not (np.array(result['Score']) != np.array(example['Score']) ).any()
+		assert not (np.array(result['Max']) != np.array(example['Max']) ).any()
+		assert not (np.array(result['Min']) != np.array(example['Min']) ).any()
+		assert not (np.array(result['SiteCountActive']) != np.array(example['SiteCountActive']) ).any()
+
+
+
+def convertHealthy():
+	warnings.warn("deprecated", DeprecationWarning)
+	prefix = '../Healthy-cfDNA/'
+	files = os.listdir(prefix)
+
+	for i, file in enumerate(files):
+		print('%d/%d' % (i, len(files)))
+		nn = file.split('_')[0]
+
+		result = convert(prefix + file)
+
+		result.to_csv(prefix + '2kbTSS_' + file.split('_')[0] , sep='\t', index=False)
---
+++ src/python/mix.py
@@ -0,0 +1,231 @@
+import numpy as np
+import os
+from convertCounts2genes import convertCounts2clusteredRegions
+import sys
+import pickle
+import pandas as pd
+import json
+
+class MedseqRun():
+    def __init__(self, filename: str):
+        # read an rcsite file and return a dictionary with site id and read count
+        # only for sites with reads
+        self.counts = dict()
+        self.totalReads = 0
+        with open(filename, 'r') as f:
+            for i, line in enumerate(f):
+                c = int(line.split('\t')[0])
+
+                if c > 0:
+                    self.counts[i] = c
+                    self.totalReads += c
+
+
+
+    def sample(self, target: int, seed=None):
+        # target: number of reads to keep
+        # seed: random seed (optional)
+        # downsample a set of reads to user-specified target value, by removing reads
+
+        if target > self.totalReads:
+            print('not enough reads! Asked for %d, but sample has %d.' % (target, self.totalReads))
+            raise ValueError
+
+        if seed is not None:
+            np.random.seed(seed)
+
+        # all regions that have non-zero count
+        locations = sorted(self.counts.keys())
+
+        regions = np.zeros(self.totalReads, int)
+        current = 0
+        for l in locations:
+            regions[current:current+self.counts[l]] = l
+
+            current += self.counts[l]
+
+
+        # result is stored here
+        sampledCounts = dict()
+        samples = np.random.choice(regions, size=target, replace=False)
+
+        sampledRegions, counts = np.unique(samples, return_counts=True)
+
+        for rr, cc in zip(sampledRegions, counts):
+            sampledCounts[rr] = cc
+
+        return sampledCounts
+
+
+def writeToFile(filename, countsDict):
+    with open(filename, 'w') as fw:
+        for i in range(16596934):
+            try:
+                c = countsDict[i]
+            except KeyError:
+                c = 0
+            line = '%d\t%d\t%d\t%d\n' % (c, i, c, 0)
+            fw.write(line)
+
+
+
+def mix(samples: list, proportions: list, totalcoverage: int, seed: int):
+    # sampleFiles: a list of medseq objects to sample from
+    # proportions: list of the corresponding proportions
+    # total # methylated reads to generate in the mixture
+    # returns a dictionary of counts
+
+    assert len(samples) == len(proportions), 'Length mismatch samples and proportions'
+    proportions = np.array(proportions)
+    assert np.sum(proportions) == 1.0, 'Provided mixture proportions do not add to 1'
+
+    neededReads = (totalcoverage * proportions).astype(int)
+    # print(neededReads)
+    # assert np.min(neededReads) > 0, 'Specified proportions of at least one sample too low'
+
+    mixture = None
+    for i, (mo,r) in enumerate(zip(samples, neededReads)):
+        # if no reads required for one sample do nothing
+        if r > 0:
+            # otherwise subsample each sample and add the reads
+            # to the pile
+            sampledCounts = mo.sample(r, seed)
+
+            if mixture is None:
+                mixture = sampledCounts
+            else:
+                # add sampled read counts to existing list of read counts
+                for k,v in sampledCounts.items():
+                    if k in mixture:
+                        mixture[k] += v
+                    else:
+                        mixture[k] = v
+
+
+    actualProportions = neededReads / np.sum(neededReads)
+
+    return (mixture, list(actualProportions))
+
+
+def convert2df(countsDict: dict):
+    cc = np.zeros(16596934, int)
+    for cpg, count in countsDict.items():
+        cc[cpg] = count
+
+    return cc
+
+
+
+
+if __name__ == '__main__':
+    # neuronName = '../data/atlas/neuron_2_CpG_M_rcSite.txt'
+    # healthyName = '../../hbds/batch2/L9508_rcSite.txt'
+    #
+    # neuron = MedseqRun('../data/atlas/neuron_2_CpG_M_rcSite.txt')
+    # healthy = MedseqRun('../../hbds/batch2/L9508_rcSite.txt')
+    #
+    # mixture = mix([neuron, healthy], [0.1, 0.9], 5000000, 42)
+
+    inputPathsFile = sys.argv[1]
+    with open(inputPathsFile) as f:
+        inputPaths = json.load(f)
+
+
+    allSampleNames = []
+    allSampleCodeNames = []
+    for code, path in inputPaths.items():
+
+        tmpFiles = []
+        tmpNames = []
+        for k in sorted(os.listdir(path)):
+            tmpFiles.append(path + k)
+            # keep first part before all '_'
+            # then remove all '-'
+            cc = k.split('_')[0]
+            cc = ''.join(cc.split('-'))
+            tmpNames.append(code + cc)
+
+
+        allSampleNames.append(tmpFiles)
+        allSampleCodeNames.append(tmpNames)
+
+
+    Ntissues = len(allSampleNames)
+
+    print('reading samples...')
+    allSamples = [[MedseqRun(f) for f in ss] for ss in allSampleNames]
+
+    # allNeuronSamples = [MedseqRun(f) for f in allNeuronSampleNames]
+    # print('reading healthy samples...')
+    # allHBDSamples = [MedseqRun(f) for f in allHBDSampleNames]
+    print('starting to mix')
+
+    savePath = sys.argv[2]
+
+    # nr of mixtures
+    Nsamples = 30
+
+    # nr of repeats/seeds to be used for mixing
+    seeds = [42, 10000, 192071]
+
+    if Ntissues != 2:
+        raise NotImplementedError
+
+    # for two tissues, assume background is the second
+    # fractions = [1.0, 0.5, 0.3, 0.1, 0.075, 0.05, 0.025, 0.01, 0.005, 0.001, 0.0]
+    fractions = np.array([[1.0, 0.0], [0.5, 0.5], [0.3, 0.7], [0.1, 0.9], [0.075, 0.925], [0.01, 0.99], [0.005, 0.995], [0.001, 0.999], [0.0, 1.0]])
+
+    depths = [5000000, 10000000]
+
+
+    # 11 fractions * 30 mixtures * 3 repeats * 2 depths = 1980 fake medseq samples
+    np.random.seed(104)
+    counter = 0
+
+    fakeClusters = np.zeros(((Nsamples * len(fractions) * len(depths) * len(seeds)), 392443), int)
+    mixedSampleNames = []
+
+    for i in range(Nsamples):
+
+        indices = [np.random.choice(len(ss)) for ss in allSamples]
+
+        for dp in depths:
+            for nfr in fractions:
+                for seed in seeds:
+                    print(counter, flush=True)
+                    try:
+                        mixedCounts, proportions = mix([k[ii] for (k,ii) in zip(allSamples, indices)], nfr, dp, seed)
+                        #sn = '%s-%.4f-%s-%.4f-%d-%d' % (codenamesHBD[hsi], proportions[1], codenamesNeurons[nsi], proportions[0], dp, seed)
+                        sn = ''
+                        for tc, (nn, ii) in enumerate(zip(allSampleCodeNames, indices)):
+                            sn += '%s-%.4f-' % (nn[ii], proportions[tc])
+                        sn += '%d-%d' % (dp, seed)
+
+                        print(sn)
+                        clusterProfile = convert2df(mixedCounts)
+                        fakeClusters[counter] = convertCounts2clusteredRegions(clusterProfile, 0)
+
+                    except ValueError:
+                        sn = 'invalid-sim'
+
+                    mixedSampleNames.append(sn)
+                    # writeToFile(outfile, mixedCounts)
+                    counter += 1
+
+
+    with open('../resources/site2cluster.pkl', 'rb') as f:
+        mapping = pickle.load(f)
+
+    geneNames = ['' for _ in range(len(mapping))]
+
+    for ii, key in enumerate(sorted(mapping)):
+        cpgInds = mapping[key]
+        geneNames[ii] = key
+
+    df = pd.DataFrame(fakeClusters, index=mixedSampleNames, columns=geneNames)
+    # df.to_csv(savePath + 'sim1.csv')
+
+    # save in smaller files to be read in parallel more quickly
+    nfiles = 1 + (df.shape[0] // 100)
+    for i in range(nfiles):
+        df.iloc[i*100:(i+1)*100].to_csv(savePath + 'sim1_part' + str(i) + '.csv')
